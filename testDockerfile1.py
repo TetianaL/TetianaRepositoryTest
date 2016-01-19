0904243edd418bab3fe343be67d8baf8e720d7f3
Tetiana <lavrynovych.t@gmail.com>
2016-01-19 15:31:17 +0100

    added 2 testDockerfile and Dockerfile

---
+++ testDockerfile1.py
@@ -0,0 +1,22 @@
+# wurzel.py
+
+
+#a = input("Zu berechnende Quadratwurzel: ")
+
+#x = input("Startwert x1: ")
+
+#a = 2
+#x=1
+
+print ("Iteration  Näherungswert")
+
+print ("_________________________" )  
+
+for i in range(1,6):
+
+  x = 0.5*(float(x)+float(a)/float(x))
+  # Bemerkung: 0.5 statt 1/2, damit mit Kommazahlen gerechnet wird!
+
+  print ('   ',i,'    ',x)
+
+
