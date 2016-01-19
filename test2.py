f642db56a4f06fc57aee6a8e46446d0392abd3df
Tetiana <lavrynovych.t@gmail.com>
2016-01-19 20:20:40 +0100

    prints

--- test2.py
+++ test2.py
@@ -8,13 +8,13 @@
 a = 2
 x=1
 
-#print("Iteration  Näherungswert")
+print("Iteration  Näherungswert")
 
-#print("_________________________" )  
+print("_________________________" )  
 
 for i in range(1,6):
 
   x = 0.5*(float(x)+float(a)/float(x))
   # Bemerkung: 0.5 statt 1/2, damit mit Kommazahlen gerechnet wird!
 
-  #print('   ',i,'    ',x)
+  print('   ',i,'    ',x)
